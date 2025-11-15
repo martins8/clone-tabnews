3b6c6e7833caaf3170eab28accff037f1c3db6e6
arthurdev <arthurmartins.dev2025@gmail.com>
2025-11-15 13:37:54 -0300

    modifie h1 text

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
+<<<<<<< HEAD
    return <h1> alô cururu </h1>
+=======
+   return <h1> Olá sapo cururu na beira do rio </h1>
+>>>>>>> a6666aa (modifie h1 text)
 }
 
 export default Home;
