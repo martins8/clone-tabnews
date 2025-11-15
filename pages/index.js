91ffc3386728dbc68dc00958c0ced9bde45cc37a
arthurdev <arthurmartins.dev2025@gmail.com>
2025-11-15 16:40:48 -0300

    practice creating a simple page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
+function PageContent() {
+   return(
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+         <div>
+            <h1>sapo cururu na beira do rio 🐸🐸</h1>
+         </div>
+         <div>
+            <iframe width="560" height="315" 
+            src="https://www.youtube.com/embed/vrkDOY5skTw?si=l4pI4sa6iESJe4Ql" 
+            title="YouTube video player" frameBorder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerPolicy="strict-origin-when-cross-origin" allowFullScreen>            
+            </iframe>
+         </div>
+      </div>
+   )
+}
+
 function Home() {
-   return <h1> Oi você que está lendo, você está bem? :D </h1>
+   return <PageContent />;
 }
 
 export default Home;
