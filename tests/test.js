3d92f54486ff3f02a9dec1162e685526c9b2dcf0
arthurdev <arthurmartins.dev2025@gmail.com>
2025-11-21 09:25:58 -0300

    add folders: api, models, tests

--- tests/test.js
+++
@@ -1,9 +0,0 @@
-const calculadora = () => {
-  return 2 + 3;
-};
-let a = 0;
-if ((a = calculadora() !== 5)) {
-  throw new Error("Teste falhou: 2 + 3 deveria ser igual a 5");
-} else {
-  console.log("Teste passou: 2 + 3 é igual a 5");
-}
