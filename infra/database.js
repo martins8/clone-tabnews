e48725814a3e3a42f68ee377e3df39cebd14abdb
arthurdev <arthurmartins.dev2025@gmail.com>
2025-11-26 10:43:35 -0300

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,23 +7,20 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-  });
-  console.log({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
   try {
     await client.connect();
+    console.log("conexão estabelecida");
     const result = await client.query(queryObject);
+    console.log("query alcançada");
     return result;
   } catch (error) {
     console.error(error);
     throw error;
   } finally {
     await client.end();
+    console.log("conexão finalizada");
   }
 }
 
