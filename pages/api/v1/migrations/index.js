dd152742987c0de064451c8af6cc7161cebfff50
arthurdev <arthurmartins.dev2025@gmail.com>
2025-12-04 10:18:09 -0300

    force deploy

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,6 +2,7 @@ import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 import database from "infra/database.js";
 
+//force deploy
 export default async function migrations(request, response) {
   const allowedMethods = ["GET", "POST"];
   if (!allowedMethods.includes(request.method)) {
