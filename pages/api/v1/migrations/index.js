41febf927c8e27e04209c5c26e9bb5d268732249
arthurdev <arthurmartins.dev2025@gmail.com>
2026-03-18 11:07:19 -0300

    refactor: improve code

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,33 +2,13 @@ import { createRouter } from "next-connect";
 import migrationRunner from "node-pg-migrate";
 import { resolve } from "node:path";
 import database from "infra/database.js";
-import { InternalServerError, MethodNotAllowedError } from "infra/errors.js";
-
+import { errorHandlers } from "infra/controller.js";
 const router = createRouter();
 
 router.get(getHandler);
 router.post(postHandler);
 
-export default router.handler({
-  onNoMatch: onNoMatchHandler,
-  onError: onErrorHandler,
-});
-
-function onErrorHandler(error, request, response) {
-  const publicErrorObject = new InternalServerError({
-    cause: error,
-  });
-
-  console.log("\n Erro dentro do catch do next-connect");
-  console.error(publicErrorObject);
-
-  response.status(500).json(publicErrorObject);
-}
-
-function onNoMatchHandler(request, response) {
-  const publicErrorObject = new MethodNotAllowedError();
-  response.status(publicErrorObject.statusCode).json(publicErrorObject);
-}
+export default router.handler(errorHandlers);
 
 const defaultMigrationOptions = {
   dryRun: true,
